f2e2fd836ead1d98dca84268885e1bb9245c361a
ryachen <31374146+ryachen@users.noreply.github.com>
2017-09-09 10:22:15 -0700

    Add files via upload

--- circles.py
+++ circles.py
@@ -3,16 +3,18 @@ import random
 
 game_objects = []
 
-class Circle:
+class Shape:
     def __init__(self, x, y):
         '''Create a new circle at the given x,y point with a random speed, color, and size.'''
-
- 
-
         self.x = x
         self.y = y
         self.x_speed = random.randint(-5,5)
         self.y_speed = random.randint(-5,5)
+
+        if (self.x_speed == 0) and (self.y_speed == 0):
+            self.x_speed == 1
+            self.y_speed == 1
+
         # this creates a random hex string between #000000 and #ffffff
         # we draw it with an outline, so we'll be able to see it on a white background regardless
         self.color = '#{0:0>6x}'.format(random.randint(00,16**6))
@@ -35,36 +37,75 @@ class Circle:
             self.y_speed = random.randint(-5,0)
 
 
-       
-            
-            
-        
-        
+class Circle(Shape):
 
-            
     def draw(self, canvas):
         '''Draw self on the canvas.'''
-
-        if self.shape == 1:
-            canvas.create_oval(self.x, self.y, self.x + self.size, self.y + self.size,
+        canvas.create_oval(self.x, self.y, self.x + self.size, self.y + self.size,
                            fill=self.color, outline="black")
+
+class Square(Shape):
+    def draw(self, canvas):
+        '''Draw self on the canvas.'''
+        canvas.create_rectangle(self.x, self.y, self.x + self.size, self.y + self.size,
+                           fill=self.color, outline="black")
+class SpecialSquare(Shape):
+
+    def update(self):
+        '''Update current location by speed.'''
+
+        self.x += self.x_speed
+        self.y += self.y_speed
+
+        if self.x_speed < 20 and self.y_speed < 20:
+            if self.x < 0 :
+                self.x_speed = self.x_speed * -1.4
+            if self.x > (400 - self.size) :
+                self.x_speed = self.x_speed * -1.4
+            if self.y < 0:
+                self.y_speed = self.y_speed * -1.4
+            if self.y > (400 - self.size):
+                self.y_speed = self.y_speed * -1.4
         else:
-            canvas.create_rectangle(self.x, self.y, self.x + self.size, self.y + self.size,
-                   fill=self.color, outline="black")
-
-       
+            self.x_speed = 5
+            self.y_speed = 5
 
 
-def addCircle(event):
+
+
+    
+    def draw(self, canvas):
+
+
+
+        canvas.create_rectangle(self.x, self.y, self.x + self.size, self.y + self.size,
+                           fill=self.color, outline="black")
+
+        
+def addShape(event):
     '''Add a new circle where the user clicked.'''
 
     global game_objects
 
-    circlex = random.randint(0,350)
-    circley = random.randint(0,350)
+    shapeX = random.randint(0,350)
+    shapeY = random.randint(0,350)
 
- 
-    game_objects.append(Circle(circlex, circley))
+    
+    constructor = random.choice((Circle, Square))
+    game_objects.append(constructor(shapeX, shapeY))
+
+
+def addSpecialSquare(event):
+    '''Add a new circle where the user clicked.'''
+
+    global game_objects
+
+
+
+
+    x = random.randint(0,350)
+    y = random.randint(0,350)
+    game_objects.append(SpecialSquare(x, y))
     
  
     
@@ -104,7 +145,10 @@ if __name__ == '__main__':
 
     # if the user presses a key or the mouse, call our handlers
     root.bind('<Key-r>', reset)
-    root.bind('<Key-space>', addCircle)
+    root.bind('<Key-space>', addShape)
+    root.bind('<Key-space>', addShape)
+    root.bind('<Key-c>', addSpecialSquare)
+ 
 
     # start the draw loop
     draw(canvas)
